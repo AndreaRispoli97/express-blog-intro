ee7521fbd84ba9577b85e74d8cce8fb631f83920
AndreaRispoli97 <Andrea9715@outlook.it>
2025-03-26 15:22:53 +0100

    add:aggiunto funzione server "express" e la "port"

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log('Andrea');
+
+
+const express = require('express')
+const app = express()
+const port = 3500
+app.get('/', (req, res) => {
+res.send('Hello World!')
+console.log('Andrea')
+})
+app.listen(port, () => {
+    console.log(`La mia porta è ${port}`)
+    })
